9cab0ccc4564163d408cb88014e260c54af729c5
Koboo <14143908+Koboo@users.noreply.github.com>
2022-03-16 14:04:30 +0100

    Implement OperatorConfig

---
+++ basic-server/src/main/java/eu/koboo/minestom/server/listener/PlayerLoginListener.java
@@ -0,0 +1,24 @@
+package eu.koboo.minestom.server.listener;
+
+import eu.koboo.minestom.server.ServerImpl;
+import java.util.function.Consumer;
+import lombok.AllArgsConstructor;
+import net.minestom.server.coordinate.Pos;
+import net.minestom.server.entity.Player;
+import net.minestom.server.event.player.PlayerLoginEvent;
+import net.minestom.server.instance.InstanceContainer;
+
+@AllArgsConstructor
+public class PlayerLoginListener implements Consumer<PlayerLoginEvent> {
+
+    private final InstanceContainer instanceContainer;
+
+    @Override
+    public void accept(PlayerLoginEvent event) {
+        event.setSpawningInstance(instanceContainer);
+        event.getPlayer().setRespawnPoint(new Pos(0, 44, 0));
+        if(ServerImpl.getInstance().isOperator(event.getPlayer())) {
+            event.getPlayer().setPermissionLevel(4);
+        }
+    }
+}
