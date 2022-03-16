2f97f9c42e8a76767463a55e7df5601045f32483
Koboo <14143908+Koboo@users.noreply.github.com>
2022-03-16 10:03:08 +0100

    Update CommandStop

--- basic-server/src/main/java/eu/koboo/minestom/server/commands/CommandStop.java
+++ basic-server/src/main/java/eu/koboo/minestom/server/commands/CommandStop.java
@@ -2,14 +2,12 @@ package eu.koboo.minestom.server.commands;
 
 import eu.koboo.minestom.server.utilities.Permissions;
 import net.minestom.server.MinecraftServer;
-import net.minestom.server.command.ConsoleSender;
 import net.minestom.server.command.builder.Command;
 import net.minestom.server.entity.Player;
-import net.minestom.server.network.packet.client.ClientPacketsHandler.Play;
 
-public final class StopCommand extends Command {
+public class CommandStop extends Command {
 
-    public StopCommand() {
+    public CommandStop() {
         super("stop", "end", "shutdown");
         setCondition((sender, command) -> {
             if(sender instanceof Player && !sender.hasPermission(Permissions.COMMAND_STOP)) {
