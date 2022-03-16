f46cd09d3a2ba4c5d2926def85ecdd881c21af6d
Koboo <14143908+Koboo@users.noreply.github.com>
2022-03-16 10:03:32 +0100

    Add more Permissions to utility object

--- basic-server/src/main/java/eu/koboo/minestom/server/utilities/Permissions.java
+++ basic-server/src/main/java/eu/koboo/minestom/server/utilities/Permissions.java
@@ -3,10 +3,19 @@ package eu.koboo.minestom.server.utilities;
 public class Permissions {
 
     public static final String COMMAND_STOP = "permission.command.stop";
+
     public static final String COMMAND_GAMEMODE = "permission.command.gamemode";
     public static final String COMMAND_GAMEMODE_OTHER = "permission.command.gamemode.other";
+
     public static final String COMMAND_TELEPORT = "permission.command.teleport";
     public static final String COMMAND_TELEPORT_OTHER = "permission.command.teleport.other";
+    public static final String COMMAND_TELEPORT_HERE = "permission.command.teleport.here";
+
+    public static final String COMMAND_SPAWN = "permission.command.spawn";
+    public static final String COMMAND_SPAWN_OTHER = "permission.command.spawn.other";
+
+    public static final String COMMAND_FLY = "permission.command.fly";
+    public static final String COMMAND_FLY_OTHER = "permission.command.fly.other";
 
 
     public static final String MESSAGE_NO_PERMISSION = "You don't have permissions to do that.";
