3ec28c73ea4e4b8661c79d9bc2231ec5ee04fd35
Koboo <14143908+Koboo@users.noreply.github.com>
2022-03-16 13:48:04 +0100

    Update filePath of CommandConfig

--- basic-server/src/main/java/eu/koboo/minestom/config/CommandConfig.java
+++ basic-server/src/main/java/eu/koboo/minestom/config/CommandConfig.java
@@ -14,22 +14,21 @@ import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
-public class CommandConfig {
+public record CommandConfig(List<String> commandNames) {
 
-    private final List<String> commandNames;
+    private static final String FILE_PATH = "configs/commands.txt";
 
     @SuppressWarnings("all")
     public static CommandConfig load() {
-        String filePath = "configs/commands.yml";
         List<String> content = new ArrayList<>();
         try {
-            File file = new File(filePath);
+            File file = new File(FILE_PATH);
             if (file.exists()) {
                 BufferedReader reader = new BufferedReader(
                         new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8));
                 String line;
                 while ((line = reader.readLine()) != null) {
-                    if(line.startsWith("#")) {
+                    if (line.startsWith("#")) {
                         continue;
                     }
                     content.add(line.toLowerCase(Locale.ROOT));
@@ -37,7 +36,7 @@ public class CommandConfig {
                 reader.close();
             } else {
                 file.createNewFile();
-                PrintWriter writer = new PrintWriter(filePath, StandardCharsets.UTF_8);
+                PrintWriter writer = new PrintWriter(FILE_PATH, StandardCharsets.UTF_8);
                 writer.println("# These are all commands provided by the server.");
                 writer.println("# If you want to disable one command");
                 writer.println("# just remove it from the list or comment out with '#'.");
