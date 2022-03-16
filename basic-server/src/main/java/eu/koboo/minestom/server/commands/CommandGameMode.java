f428acdc4dc17e7187421ec3b3fd12368aa7e0f5
Koboo <14143908+Koboo@users.noreply.github.com>
2022-03-16 10:03:22 +0100

    Update CommandGameMode

--- basic-server/src/main/java/eu/koboo/minestom/server/commands/CommandGameMode.java
+++ basic-server/src/main/java/eu/koboo/minestom/server/commands/CommandGameMode.java
@@ -1,10 +1,12 @@
 package eu.koboo.minestom.server.commands;
 
+import eu.koboo.minestom.server.utilities.Arguments;
 import eu.koboo.minestom.server.utilities.Permissions;
 import net.minestom.server.command.CommandSender;
 import net.minestom.server.command.ConsoleSender;
 import net.minestom.server.command.builder.Command;
 import net.minestom.server.command.builder.CommandContext;
+import net.minestom.server.command.builder.arguments.Argument;
 import net.minestom.server.command.builder.arguments.ArgumentEnum;
 import net.minestom.server.command.builder.arguments.ArgumentType;
 import net.minestom.server.command.builder.arguments.minecraft.ArgumentEntity;
@@ -15,34 +17,34 @@ import net.minestom.server.network.packet.client.ClientPacketsHandler.Play;
 import net.minestom.server.utils.entity.EntityFinder;
 import org.jetbrains.annotations.NotNull;
 
-public class GameModeCommand extends Command {
+public class CommandGameMode extends Command {
 
-    public GameModeCommand() {
+    @SuppressWarnings("all")
+    public CommandGameMode() {
         super("gamemode", "gm");
 
-        setCondition((sender, command) -> {
-            if(sender instanceof ConsoleSender) {
-                sender.sendMessage("Only for the players.");
-                return false;
-            }
-            return true;
-        });
+        setCondition(Conditions::playerOnly);
 
         ArgumentEnum<GameMode> gameMode = ArgumentType.Enum("gameMode", GameMode.class)
                 .setFormat(ArgumentEnum.Format.LOWER_CASED);
 
-        ArgumentEntity other = ArgumentType.Entity("player").onlyPlayers(true).singleEntity(true);
+        Argument<GameMode> gameModeId = ArgumentType.Integer("gameModeId").between(0, 3).map(id -> GameMode.values()[id]);
 
-        addSyntax(this::executeSelf, gameMode);
-        addSyntax(this::executeOther, gameMode, other);
+        addSyntax((sender, context) -> executeSelf(sender, context.get("gameMode")), gameMode);
+        addSyntax((sender, context) -> executeSelf(sender, context.get("gameModeId")), gameModeId);
+        addSyntax((sender, context) -> executeOther(sender, context.get("target"), context.get("gameMode")), gameMode, Arguments.TARGET);
+        addSyntax((sender, context) -> executeOther(sender, context.get("target"), context.get("gameModeId")), gameModeId, Arguments.TARGET);
     }
 
-    private void executeSelf(@NotNull CommandSender sender, @NotNull CommandContext context) {
-        GameMode gamemode = context.get("gameMode");
-
+    private void executeSelf(@NotNull CommandSender sender, GameMode gamemode) {
         Player player = (Player) sender;
         if (!player.hasPermission(Permissions.COMMAND_GAMEMODE)) {
             player.sendMessage(Permissions.MESSAGE_NO_PERMISSION);
+            return;
+        }
+
+        if(gamemode == null) {
+            player.sendMessage("Syntax: /gamemode <creative | survival | adventure | spectator>");
             return;
         }
 
@@ -50,25 +52,27 @@ public class GameModeCommand extends Command {
         sender.sendMessage("Set own game mode to " + gamemode.name().toLowerCase() + " Mode");
     }
 
-    private void executeOther(@NotNull CommandSender sender, @NotNull CommandContext context) {
-        GameMode gamemode = context.get("gamemode");
+    private void executeOther(@NotNull CommandSender sender, Player target, GameMode gamemode) {
         Player player = (Player) sender;
-
         if (!player.hasPermission(Permissions.COMMAND_GAMEMODE_OTHER)) {
             player.sendMessage(Permissions.MESSAGE_NO_PERMISSION);
             return;
         }
 
-        EntityFinder targetFinder = context.get("player");
-        Player target = targetFinder.findFirstPlayer(sender);
+        if(gamemode == null) {
+            player.sendMessage("Syntax: /gamemode <creative | survival | adventure | spectator>");
+            return;
+        }
 
         if (target == null) {
-            sender.sendMessage("The target is unavailable.");
+            player.sendMessage("The target is unavailable.");
             return;
         }
 
         target.setGameMode(gamemode);
-        sender.sendMessage(
+        target.sendMessage("Set own game mode to " + gamemode.name().toLowerCase() + " Mode");
+
+        player.sendMessage(
                 "Set " + target.getUsername() + "'s game mode to " + gamemode.name().toLowerCase() + " Mode");
     }
 }
