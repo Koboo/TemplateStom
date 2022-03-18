3fa8bdda30229ed48ff979cc545a8c544274f2cd
Koboo <14143908+Koboo@users.noreply.github.com>
2022-03-18 10:29:35 +0100

    Reformat ServerConfig

--- server-api/src/main/java/eu/koboo/minestom/api/config/ServerConfig.java
+++ server-api/src/main/java/eu/koboo/minestom/api/config/ServerConfig.java
@@ -3,20 +3,10 @@ package eu.koboo.minestom.api.config;
 import net.minestom.server.entity.GameMode;
 import net.minestom.server.world.Difficulty;
 
-public record ServerConfig(String host, int port,
-                           boolean onlineMode,
-                           Difficulty difficulty,
+public record ServerConfig(String host, int port, boolean onlineMode, Difficulty difficulty,
                            GameMode gameMode,
-
-                           ProxyMode proxyMode,
-                           String velocitySecret,
-
-                           int packetRateLimit,
-                           int maxPacketSize,
-                           int compressionThreshold,
-
-                           int chunkViewDistance,
-                           int entityViewDistance
-                           ) {
+                           ProxyMode proxyMode, String velocitySecret,
+                           int packetRateLimit, int maxPacketSize, int compressionThreshold,
+                           int chunkViewDistance, int entityViewDistance) {
 
 }
