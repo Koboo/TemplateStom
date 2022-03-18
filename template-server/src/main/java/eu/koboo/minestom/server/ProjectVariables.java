934b96915d6cd104de7a051807a3c27a560c57a9
Koboo <14143908+Koboo@users.noreply.github.com>
2022-03-18 08:15:32 +0100

    Add blossom gradle plugin

---
+++ template-server/src/main/java/eu/koboo/minestom/server/ProjectVariables.java
@@ -0,0 +1,8 @@
+package eu.koboo.minestom.server;
+
+public class ProjectVariables {
+
+  public static final String NAME = "PROJECT_NAME";
+  public static final String VERSION = "PROJECT_VERSION";
+
+}
