bc7064cea7dfd90f034fa0a135f3800a06d71312
Keine Secrets <miethe.max@web.de>
2024-09-02 17:40:14 +0200

    Added default world files

--- server-app/src/main/java/eu/koboo/minestom/server/world/WorldManagerImpl.java
+++ server-app/src/main/java/eu/koboo/minestom/server/world/WorldManagerImpl.java
@@ -3,10 +3,39 @@ package eu.koboo.minestom.server.world;
 import eu.koboo.minestom.api.world.World;
 import eu.koboo.minestom.api.world.dimension.Dimension;
 import eu.koboo.minestom.api.world.manager.WorldManager;
+import net.minestom.server.MinecraftServer;
+import net.minestom.server.instance.InstanceContainer;
+import net.minestom.server.instance.anvil.AnvilLoader;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
+import java.util.Map;
 
 public class WorldManagerImpl implements WorldManager {
+
+    Map<String, World> loadedWorlds;
+    Map<String, InstanceContainer> loadedInstances;
+
     @Override
     public World createWorld(String name, Dimension dimensionType) {
+        World createdWorld = new World();
+        Path dir = Path.of("worlds/" + name);
+        if (Files.exists(dir)) {
+            InstanceContainer createdInstance = MinecraftServer.getInstanceManager().createInstanceContainer(new AnvilLoader("worlds/" + name));
+            createdWorld.setName(name);
+            createdWorld.setInstanceContainer(createdInstance);
+            createdWorld.setDimensionType(dimensionType.getDimensionType());
+            loadedWorlds.put(name, createdWorld);
+            loadedInstances.put(name, createdInstance);
+            return createdWorld;
+        }
+        try {
+            Files.createDirectory(dir);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
