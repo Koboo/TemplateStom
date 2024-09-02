d9c1dbe0ca2531a85881554480c45b5feec3e2b5
Keine Secrets <miethe.max@web.de>
2024-09-02 13:35:11 +0200

    Update authorVersion to 1.0.2

--- server-app/src/main/java/eu/koboo/minestom/server/ProjectVariables.java
+++ server-app/src/main/java/eu/koboo/minestom/server/ProjectVariables.java
@@ -3,7 +3,7 @@ package eu.koboo.minestom.server;
 public class ProjectVariables {
 
   public static final String NAME = "TemplateStom";
-  public static final String VERSION = "1.1";
+  public static final String VERSION = "1.0.2";
   public static final String MINESTOM_VERSION = "8f46913486";
 
 }
