c8b020c10b3a132e5f37a68dff204d7e64008812
Keine Secrets <miethe.max@web.de>
2024-09-02 13:15:15 +0200

    Refactored ServerConfig, removed unnecessary fields

--- server-api/src/main/java/eu/koboo/minestom/api/config/ServerConfig.java
+++ server-api/src/main/java/eu/koboo/minestom/api/config/ServerConfig.java
@@ -7,7 +7,7 @@ import net.minestom.server.world.Difficulty;
  */
 public record ServerConfig(String host, int port, boolean onlineMode, Difficulty difficulty,
                            ProxyMode proxyMode, String velocitySecret,
-                           int packetRateLimit, int maxPacketSize, int compressionThreshold,
+                           int compressionThreshold,
                            int chunkViewDistance, int entityViewDistance) {
 
 }
