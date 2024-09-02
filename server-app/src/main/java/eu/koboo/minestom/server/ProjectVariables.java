2530b510f2f83c4b643efcb1971e2c76ac6dc111
Keine Secrets <miethe.max@web.de>
2024-09-02 13:06:50 +0200

    Inserted right variables in ProjectVariables

--- server-app/src/main/java/eu/koboo/minestom/server/ProjectVariables.java
+++ server-app/src/main/java/eu/koboo/minestom/server/ProjectVariables.java
@@ -2,8 +2,8 @@ package eu.koboo.minestom.server;
 
 public class ProjectVariables {
 
-  public static final String NAME = "PROJECT_NAME";
-  public static final String VERSION = "PROJECT_VERSION";
-  public static final String MINESTOM_VERSION = "DEP_MINESTOM_VERSION";
+  public static final String NAME = "TemplateStom";
+  public static final String VERSION = "1.1";
+  public static final String MINESTOM_VERSION = "8f46913486";
 
 }
